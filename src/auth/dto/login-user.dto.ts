5da24f8ae06343be2103d893405551b0ab6770f1
jhonpulido1990 <3825@holbertonstudents.com>
2025-05-29 23:08:04 -0500

    nuevas validaciones

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class LoginUserDto {
   @IsEmail(
@@ -14,5 +20,9 @@ export class LoginUserDto {
   @IsString({ message: 'La contraseña debe ser una cadena de texto.' })
   @IsNotEmpty({ message: 'La contraseña no debe estar vacía.' })
   @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres.' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\S]{8,}$/, {
+    message:
+      'La contraseña debe contener al menos una mayúscula, una minúscula y un número.',
+  })
   password: string;
 }
--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterUserDto {
   @IsString({ message: 'El nombre debe ser una cadena de texto.' })
@@ -17,5 +23,9 @@ export class RegisterUserDto {
   @IsString({ message: 'La contraseña debe ser una cadena de texto.' })
   @IsNotEmpty({ message: 'La contraseña no debe estar vacía.' })
   @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres.' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\S]{8,}$/, {
+    message:
+      'La contraseña debe contener al menos una mayúscula, una minúscula y un número.',
+  })
   password: string;
 }
